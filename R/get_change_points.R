5289ad6c7faa1004e3585edb405b53844ee238a6
Ondrej Mottl <ondrej.mottl@gmail.com>
2022-06-09 10:24:20 +0200

    function to Detect change points in a vector

---
+++ R/get_change_points.R
@@ -0,0 +1,45 @@
+#' @title Detect change points in a vector
+#' @param data_source Vector with values (numeric, character, or factor)
+#' @param direction Which direction should the values be compared ("front" or "back")
+#' @return Vector with binary values of length = `data_source` + 1.
+#' `1` represent change point
+#' @export
+get_change_points <-
+  function(data_source,
+           direction = c("front", "back")) {
+
+    util_check_class("data_source", c("numeric", "character", "factor"))
+
+    util_check_class("direction", "character")
+
+    direction <- match.arg(direction)
+
+    util_check_vector_values("direction", c("front", "back"))
+
+    if (
+      class(data_source) %in% c("character", "factor")
+    ) {
+      data_numeric <-
+        data_source %>%
+        as.factor() %>%
+        as.numeric()
+    } else {
+      data_numeric <-
+        data_source %>%
+        as.numeric()
+    }
+
+    if (
+      direction == "front"
+    ) {
+      delta <-
+        c(FALSE, (diff(data_numeric) != 0)) * 1
+    } else if (
+      direction == "back"
+    ) {
+      delta <-
+        c((diff(data_numeric) != 0), FALSE) * 1
+    }
+
+    return(delta)
+  }
