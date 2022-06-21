71902bbee580f5bc1dff853173dcd00c64e6815d
Ondrej Mottl <ondrej.mottl@gmail.com>
2022-06-21 16:03:04 +0200

    Taxonomic diversity - use only rarefaction method. - rename the function to taxonomic

--- R/diversity_estimate.R
+++ R/diversity_estimate.R
@@ -1,6 +1,7 @@
 #' @title Estimation of interpolation and extrapolation of individual-based Hill number
-#' @param data_source Data frame with rows as levels and columns as taxa
-#' @param sel_method Selected method of diversify estimation c("randomised", "rarefy")
+#' @param data_source Data frame with rows as levels and columns as taxa. First
+#' row should be named `sample_id`.
+#' @param sel_method Selected method of diversify estimation c("taxonomic_randomised", "taxonomic_rarefy")
 #' @param sample_size minimum sample size
 #' @param rand number of randomisations
 #' @description Estimation of interpolation and extrapolation of individual-based Hill number
@@ -8,73 +9,132 @@
 #' @export
 diversity_estimate <-
   function(data_source,
-           sel_method = c("rarefy", "randomised"),
+           sel_method = c("taxonomic",
+                          "phylogenetic_diversity",
+                          "phylogenetic_nri",
+                          "phylogenetic_nti"),
            sample_size,
-           rand = 999) {
+           rand = 999,
+           iterations = 1000,
+           abundance_weighted = TRUE,
+           phylogenetic_tree = NULL) {
 
     util_check_class("data_source", "data.frame")
 
-    # remove sample_id, round down, and tranform into matrix
+    util_check_col_names("data_source", "sample_id")
+
+    # remove sample_id, round down, and transform into matrix
     data_matrix <-
       data_source %>%
-      dplyr::select(!dplyr::any_of("sample_id")) %>%
+      tibble::column_to_rownames("sample_id") %>%
       dplyr::mutate_all(., .f = floor) %>%
-      as.matrix()
+      as.matrix() %>%
+      round()
 
     sel_method <- match.arg(sel_method)
 
     util_check_class("sel_method", "character")
 
-    util_check_vector_values("sel_method", c("rarefy", "randomised"))
+    util_check_vector_values(
+      "sel_method",
+      c("taxonomic",
+        "phylogenetic_diversity",
+        "phylogenetic_nri",
+        "phylogenetic_nti")
+    )
 
-    if(missing(sample_size)){
-      sample_size <-
-        apply(data_matrix, 1, sum) %>%
-        floor() %>%
-        min()
-    }
-
-    util_check_class("sample_size", "numeric")
+    util_check_class("rand", "numeric")
 
     assertthat::assert_that(
-      round(sample_size) == sample_size,
-      msg = "'sample_size' has be integer")
+      rand ==  round(rand),
+      msg = "'rand' must be a 'integer'")
 
-    if(sel_method == "randomised") {
+    if
+    (
+      ncol(data_matrix) < 2
+    ) {
+      cat("Dataset does not have enough taxons to estimate diversity", "\n")
 
-      util_check_class("rand", "numeric")
+      return(NA)
+    }
+
+    if
+    (
+      sel_method == "taxonomic"
+    ) {
+
+      if
+      (
+        missing(sample_size)
+      ) {
+        sample_size <-
+          apply(data_matrix, 1, sum) %>%
+          floor() %>%
+          min()
+      }
+
+      util_check_class("sample_size", "numeric")
 
       assertthat::assert_that(
-        rand ==  round(rand),
-        msg = "'rand' must be a 'integer'")
+        round(sample_size) == sample_size,
+        msg = "'sample_size' has be integer")
 
       div <-
-        diversity_estimate_randomise(
-          data_matrix = round(data_matrix),
-          sample_size = sample_size,
-          set_margin = 1,
-          rand = rand)
+        diversity_estimate_taxonomic(
+          data_matrix = data_matrix,
+          sample_size = sample_size
+        )
 
-    } else if (sel_method == "rarefy"){
-      div <-
-        diversity_estimate_rarefy(
-          data_matrix = round(data_matrix),
-          sample_size = sample_size,
-          set_margin = 1)
+    } else if
+    (
+      sel_method == "phylogenetic_diversity"
+    ) {
+
+      diversity_estimate_phylogenetic(
+        data_source = data_matrix,
+        method = "diversity",
+        abundance_weighted = abundance_weighted,
+        rand = rand,
+        iterations = iterations,
+        phylogenetic_tree = phylogenetic_tree
+      )
+
+    } else if
+    (
+      sel_method == "phylogenetic_nri"
+    ) {
+
+      diversity_estimate_phylogenetic(
+        data_source = data_matrix,
+        method = "NRI",
+        abundance_weighted = abundance_weighted,
+        rand = rand,
+        iterations = iterations,
+        phylogenetic_tree = phylogenetic_tree
+      )
+
+    } else if
+    (
+      sel_method == "phylogenetic_nti"
+    ) {
+
+      diversity_estimate_phylogenetic(
+        data_source = data_matrix,
+        method = "NTI",
+        abundance_weighted = abundance_weighted,
+        rand = rand,
+        iterations = iterations,
+        phylogenetic_tree = phylogenetic_tree
+      )
+
     }
 
     res <-
       div %>%
+      tibble::rownames_to_column("sample_id") %>%
+      dplyr::relocate(sample_id) %>%
       as.data.frame() %>%
       tibble::as_tibble()
 
-    # add sample_id if present in the original data
-    if("sample_id" %in% names(data_source)){
-      res <-
-        res %>%
-        dplyr::mutate(
-          sample_id = data_source$sample_id) %>%
-        dplyr::relocate(sample_id)
-    }
     return(res)
   }
--- R/diversity_estimate_taxonomic.R
+++ R/diversity_estimate_taxonomic.R
@@ -1,20 +1,21 @@
-#' @title Estimation of interpolation and extrapolation of individual-based Hill number
-#' @param data_matrix Data matrix
-#' @param sample_size minimum sample size
-#' @param set_margin Are levels as columns or rows? 1 = rows, 2 = columns
-#' @return Matrix with Hills number N0, N1, and N2, evenness ratios, and error estimates
-#' @description Estimation of interpolation and extrapolation of individual-based Hill number
-diversity_estimate_rarefy <-
+#' @title Estimation taxonomic diversity
+#' @param data_matrix Data matrix. Rows as levels and columns as taxa. Row names
+#' should be the `sample_id`.
+#' @param sample_size Numeric. Minimum sample size
+#' @return Data frame with Hills number N0, N1, and N2, evenness ratios, and error estimates
+#' @description Estimation of interpolation and extrapolation of individual-based Hill numbers
+diversity_estimate_taxonomic <-
   function(data_matrix,
-           sample_size,
-           set_margin = 1){
+           sample_size
+  ){
 
     util_check_class("data_matrix", "matrix")
 
-    util_check_class("set_margin", "numeric")
-
-    if(missing(sample_size)){
-      sample_size <- min(apply(data_matrix, set_margin, sum))
+    if
+    (
+      missing(sample_size)
+    ) {
+      sample_size <- min(apply(data_matrix, 1, sum))
     }
 
     util_check_class("sample_size", "numeric")
@@ -28,9 +29,12 @@ diversity_estimate_rarefy <-
       function(data, sample_size){
         data_sub <- data[data > 0]
         data_sum <- sum(data_sub)
-        if(sample_size <= data_sum){
-          res <- sum(1-exp(lchoose(data_sum - data_sub, sample_size) -
-                             lchoose(data_sum, sample_size)))
+        if
+        (
+          sample_size <= data_sum
+        ) {
+          res <- sum(1 - exp(lchoose(data_sum - data_sub, sample_size) -
+                               lchoose(data_sum, sample_size)))
           return(res)
         } else {
           return(0)
@@ -41,7 +45,10 @@ diversity_estimate_rarefy <-
       function(data, sample_size){
         data_sub <- data[data > 0]
         data_sum <- sum(data_sub)
-        if(sample_size <= data_sum){
+        if
+        (
+          sample_size <= data_sum
+        ) {
           sub <-
             function(k) sum(exp(lchoose(data_sub, k) +
                                   lchoose(data_sum - data_sub, sample_size - k) -
@@ -57,11 +64,14 @@ diversity_estimate_rarefy <-
       function(data, sample_size){
         data_sub <- data[data > 0]
         data_sum <- sum(data_sub)
-        if(sample_size <= data_sum){
+        if
+        (
+          sample_size <= data_sum
+        ) {
           k <- 1:sample_size
           res <-
-            exp(-sum(k/sample_size *
-                       log(k/ sample_size) *
+            exp(-sum(k / sample_size *
+                       log(k / sample_size) *
                        fk.hat(data_sub, sample_size)))
           return(res)
         } else {
@@ -73,7 +83,10 @@ diversity_estimate_rarefy <-
       function(data, sample_size){
         data_sub <- data[data > 0]
         data_sum <- sum(data_sub)
-        if(sample_size <= data_sum){
+        if
+        (
+          sample_size <= data_sum
+        ) {
           res <- 1 / (1 / sample_size +
                         (1 - 1 / sample_size) *
                         sum(data_sub * (data_sub - 1) / data_sum / (data_sum - 1)))
@@ -87,24 +100,23 @@ diversity_estimate_rarefy <-
     est_n_0 <-
       sapply(sample_size,
              function(n) apply(data_matrix,
-                               set_margin,
+                               1,
                                hill0,
                                sample_size = n))
 
     est_n_1 <-
       sapply(sample_size,
              function(n) apply(data_matrix,
-                               set_margin,
+                               1,
                                hill1,
                                sample_size = n))
 
     est_n_2 <-
       sapply(sample_size,
              function(n) apply(data_matrix,
-                               set_margin,
+                               1,
                                hill2,
                                sample_size = n))
-
 
     # bind results
     hill_diversity <-
@@ -114,10 +126,13 @@ diversity_estimate_rarefy <-
             est_n_1 - est_n_2,
             est_n_2 / est_n_1,
             est_n_1 / est_n_0) %>%
-      tibble::as_tibble()
-
-    names(hill_diversity) <-
-      c("n0", "n1", "n2", "n1_minus_n2", "n2_divided_by_n1", "n1_divided_by_n0")
+      as.data.frame() %>%
+      tibble::as_tibble() %>%
+      dplyr::mutate(sample_id = row.names(data_matrix)) %>%
+      tibble::column_to_rownames("sample_id") %>%
+      purrr::set_names(
+        nm = c("n0", "n1", "n2", "n1_minus_n2", "n2_divided_by_n1", "n1_divided_by_n0")
+      )
 
     return(hill_diversity)
   }
