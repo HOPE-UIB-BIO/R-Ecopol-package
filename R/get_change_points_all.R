deca50450aed1cc213bb068defed912ac8bab6be
Ondrej Mottl <ondrej.mottl@gmail.com>
2022-06-23 11:59:38 +0200

    add argument to select the prefered output

--- R/get_change_points_all.R
+++ R/get_change_points_all.R
@@ -2,11 +2,20 @@
 #' @param data_source Data.frame with columns to detect change points in
 #' @param var_name_order Character. Name of the columns, which should be use
 #' as a order of values
-#' @param use_inbetween_values Logical. If `TRUE` additional values will be placed
+#' @param use_inbetween_values
+#' Logical. If `TRUE` additional values will be placed
 #' in between values of `var_name_order`.
+#' #' @param sel_output
+#' Character. Only used for `use_inbetween_values` == `TRUE`
+#' Define what should the function output.
+#' \itemize{
+#' \item `"only_inbetween"` - only inbetween values,
+#' \item `"both"` - both inbetween values and original data
+#' }
 #' @param var_name_detection Character. Columns to use to change point detection
 #' will be automatically selected if they partly matched `var_name_detection`
-#' @param direction Which direction should the values be compared ("front" or "back")
+#' @param direction
+#' Which direction should the values be compared ("front" or "back")
 #' @return Data.frame with `var_name_order` and all columns detected based on
 #' `var_name_detection`, with `1` represent change points.
 #' @export
@@ -14,12 +23,14 @@ get_change_points_all <-
   function(data_source,
            var_name_order = "age",
            use_inbetween_values = TRUE,
+           sel_output = c("only_inbetween", "both"),
            var_name_detection = "_part",
            direction = c("front", "back")) {
-
     util_check_class("data_source", "data.frame")
 
     util_check_class("var_name_order", "character")
+
+    util_check_class("use_inbetween_values", "logical")
 
     util_check_class("var_name_detection", "character")
 
@@ -43,13 +54,30 @@ get_change_points_all <-
     if (
       use_inbetween_values == TRUE
     ) {
-      data_position <-
+      util_check_class("sel_output", "character")
+
+      sel_output <- match.arg(sel_output)
+
+      util_check_vector_values("sel_output", c("only_inbetween", "both"))
+
+      data_position_work <-
         data_source %>%
         purrr::pluck(var_name_order) %>%
-        util_get_inbetween_values()
-    } else {
+        util_get_inbetween_values(
+          data_source = .,
+          sel_output = "both"
+        )
 
-      data_position <-
+      data_position_fin <-
+        data_source %>%
+        purrr::pluck(var_name_order) %>%
+        util_get_inbetween_values(
+          data_source = .,
+          sel_output = sel_output
+        )
+    } else {
+      data_position_work <-
+        data_position_fin <-
         data_source %>%
         purrr::pluck(var_name_order)
     }
@@ -58,40 +86,52 @@ get_change_points_all <-
       data_source %>%
       dplyr::select(
         dplyr::contains(
-          c("sample_id", var_name_order, var_name_detection))
+          c("sample_id", var_name_order, var_name_detection)
+        )
       ) %>%
       tidyr::pivot_longer(
         cols = !dplyr::all_of(c("sample_id", var_name_order)),
         names_to = "var",
-        values_to = "value") %>%
+        values_to = "value"
+      ) %>%
       split(.$var)
 
     purrr::map_dfr(
       .x = var_list,
       .f = ~ tibble::tibble(
-        !!var_name_order := data_position
+        !!var_name_order := data_position_work
       ) %>%
         dplyr::left_join(
           .,
           .x,
-          by = var_name_order) %>%
+          by = var_name_order
+        ) %>%
         tidyr::fill(
           value,
           .direction = switch(direction,
-                              "front" = "up",
-                              "back" = "down")) %>%
+            "front" = "up",
+            "back" = "down"
+          )
+        ) %>%
         dplyr::mutate(
           cp = get_change_points(
             as.numeric(value),
-            direction = direction)) %>%
+            direction = direction
+          )
+        ) %>%
         dplyr::select(
           dplyr::all_of(
             c(var_name_order, "cp")
           )
         ),
-      .id = "var") %>%
+      .id = "var"
+    ) %>%
       tidyr::pivot_wider(
         names_from = "var",
-        values_from = "cp") %>%
+        values_from = "cp"
+      ) %>%
+      dplyr::filter(
+        get(var_name_order) %in% data_position_fin
+      ) %>%
       return()
   }
