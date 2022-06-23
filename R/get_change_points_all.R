6d10265bae7217243b68b89874cf22412806bb1b
Ondřej Mottl <60345393+OndrejMottl@users.noreply.github.com>
2022-06-23 12:20:18 +0200

    Change-point - outputs (#15)
    * add an argument to select the prefered output
    * add posibility to select all columns

--- R/get_change_points_all.R
+++ R/get_change_points_all.R
@@ -1,12 +1,23 @@
 #' @title Detect change points for all columns
 #' @param data_source Data.frame with columns to detect change points in
-#' @param var_name_order Character. Name of the columns, which should be use
-#' as a order of values
-#' @param use_inbetween_values Logical. If `TRUE` additional values will be placed
+#' @param var_name_order Character.
+#' Name of the columns, which should be use as a order of values
+#' @param use_inbetween_values
+#' Logical. If `TRUE` additional values will be placed
 #' in between values of `var_name_order`.
-#' @param var_name_detection Character. Columns to use to change point detection
-#' will be automatically selected if they partly matched `var_name_detection`
-#' @param direction Which direction should the values be compared ("front" or "back")
+#' #' @param sel_output
+#' Character. Only used for `use_inbetween_values` == `TRUE`
+#' Define what should the function output.
+#' \itemize{
+#' \item `"only_inbetween"` - only inbetween values,
+#' \item `"both"` - both inbetween values and original data
+#' }
+#' @param var_name_detection
+#' Character. Optional. Columns to use to change point detection
+#' will be automatically selected if they partly matched `var_name_detection`.
+#' For all columns use `NULL``.
+#' @param direction
+#' Which direction should the values be compared ("front" or "back")
 #' @return Data.frame with `var_name_order` and all columns detected based on
 #' `var_name_detection`, with `1` represent change points.
 #' @export
@@ -14,14 +25,14 @@ get_change_points_all <-
   function(data_source,
            var_name_order = "age",
            use_inbetween_values = TRUE,
-           var_name_detection = "_part",
+           sel_output = c("only_inbetween", "both"),
+           var_name_detection = NULL,
            direction = c("front", "back")) {
-
     util_check_class("data_source", "data.frame")
 
     util_check_class("var_name_order", "character")
 
-    util_check_class("var_name_detection", "character")
+    util_check_class("use_inbetween_values", "logical")
 
     util_check_class("direction", "character")
 
@@ -31,67 +42,116 @@ get_change_points_all <-
 
     util_check_col_names("data_source", c(var_name_order, "sample_id"))
 
-    assertthat::assert_that(
-      stringr::str_detect(names(data_source), var_name_detection) %>%
-        any(),
-      msg = paste(
-        "'data_source' must contain at least one column which partly matched with",
-        var_name_detection
-      )
-    )
-
     if (
       use_inbetween_values == TRUE
     ) {
-      data_position <-
+      util_check_class("sel_output", "character")
+
+      sel_output <- match.arg(sel_output)
+
+      util_check_vector_values("sel_output", c("only_inbetween", "both"))
+
+      data_position_work <-
         data_source %>%
         purrr::pluck(var_name_order) %>%
-        util_get_inbetween_values()
-    } else {
+        util_get_inbetween_values(
+          data_source = .,
+          sel_output = "both"
+        )
 
-      data_position <-
+      data_position_fin <-
+        data_source %>%
+        purrr::pluck(var_name_order) %>%
+        util_get_inbetween_values(
+          data_source = .,
+          sel_output = sel_output
+        )
+    } else {
+      data_position_work <-
+        data_position_fin <-
         data_source %>%
         purrr::pluck(var_name_order)
+    }
+
+    if
+    (
+      is.null(var_name_detection) == FALSE
+    ) {
+      util_check_class("var_name_detection", "character")
+
+      assertthat::assert_that(
+        stringr::str_detect(names(data_source), var_name_detection) %>%
+          any(),
+        msg = paste(
+          "'data_source' must contain at least one column",
+          "which partly matched with",
+          util_paste_as_vector(var_name_detection)
+        )
+      )
+    } else {
+      var_name_detection <-
+        data_source %>% 
+        dplyr::select(
+          !dplyr::any_of(
+            c(
+              "sample_id",
+              var_name_order
+            )
+          )
+        ) %>%
+        names()
     }
 
     var_list <-
       data_source %>%
       dplyr::select(
         dplyr::contains(
-          c("sample_id", var_name_order, var_name_detection))
+          c("sample_id", var_name_order, var_name_detection)
+        )
       ) %>%
       tidyr::pivot_longer(
         cols = !dplyr::all_of(c("sample_id", var_name_order)),
         names_to = "var",
-        values_to = "value") %>%
+        values_to = "value"
+      ) %>%
       split(.$var)
 
     purrr::map_dfr(
       .x = var_list,
       .f = ~ tibble::tibble(
-        !!var_name_order := data_position
+        !!var_name_order := data_position_work
       ) %>%
         dplyr::left_join(
           .,
           .x,
-          by = var_name_order) %>%
+          by = var_name_order
+        ) %>%
         tidyr::fill(
           value,
           .direction = switch(direction,
-                              "front" = "up",
-                              "back" = "down")) %>%
+            "front" = "up",
+            "back" = "down"
+          )
+        ) %>%
         dplyr::mutate(
           cp = get_change_points(
             as.numeric(value),
-            direction = direction)) %>%
+            direction = direction
+          )
+        ) %>%
         dplyr::select(
           dplyr::all_of(
             c(var_name_order, "cp")
           )
         ),
-      .id = "var") %>%
+      .id = "var"
+    ) %>%
       tidyr::pivot_wider(
         names_from = "var",
-        values_from = "cp") %>%
+        values_from = "cp"
+      ) %>%
+      dplyr::filter(
+        get(var_name_order) %in% data_position_fin
+      ) %>%
       return()
   }
--- R/util_get_inbetween_values.R
+++ R/util_get_inbetween_values.R
@@ -1,35 +1,60 @@
 #' @title Get in-between values
 #' @param data_source Vector with values
+#' @param sel_output
+#' Character. Define what should the function output.
+#' \itemize{
+#' \item `"only_inbetween"` - only inbetween values,
+#' \item `"both"` - both inbetween values and original data
+#' }
 #' @description Add values in-between each pair or values in a vector. Position
 #' of each value is calculated as average of 2 original values.
 util_get_inbetween_values <-
-  function(data_source) {
-
+  function(data_source,
+           sel_output = c("only_inbetween", "both")) {
     util_check_class("data_source", "numeric")
+
+    util_check_class("sel_output", "character")
+
+    sel_output <- match.arg(sel_output)
+
+    util_check_vector_values("sel_output", c("only_inbetween", "both"))
 
     # helper function
     offdiag <-
       function(matrix, offset) {
         s <- seq(offset)
-        diag(matrix[ ,-s, drop = FALSE])
+        diag(matrix[, -s, drop = FALSE])
       }
 
     inbetween_values <-
       expand.grid(
         a = data_source,
-        b = data_source) %>%
+        b = data_source
+      ) %>%
       dplyr::mutate(
-        avg = (a + b)/2
+        avg = (a + b) / 2
       ) %>%
       tidyr::pivot_wider(names_from = b, values_from = avg) %>%
       tibble::column_to_rownames("a") %>%
       as.matrix() %>%
       offdiag(., 1)
 
-    c(
-      data_source,
-      inbetween_values
-    ) %>%
-      sort() %>%
-      return()
+    if
+    (
+      sel_output == "both"
+    ) {
+      c(
+        data_source,
+        inbetween_values
+      ) %>%
+        sort() %>%
+        return()
+    } else if
+    (
+      sel_output == "only_inbetween"
+    ) {
+      inbetween_values %>%
+        sort() %>%
+        return()
+    }
   }
