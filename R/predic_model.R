e2534306d17ae53e6943fd42669532e53a6dcfe7
Ondrej Mottl <ondrej.mottl@gmail.com>
2022-08-25 11:05:48 +0200

    refactor to use `marginaleffects::predictions`

--- R/predic_model.R
+++ R/predic_model.R
@@ -1,36 +1,53 @@
-#' Predict data from a linear model
+#' Predict data from a regression model
 #'
 #' @param data_source Data.frame with values to be predicted upon
-#' @param model_source Selected model. Can be `lm`, `glm`, or `gam`
+#' @param model_source Selected model. Can be any of supported models by
+#' `marginaleffects`
 #' @param exclude_var Vector with terms to exclude. This is used only for `gam`
 #' models
+#' @description Predict model using `marginaleffects::predictions` function
 #' @export
 predic_model <-
-  function(data_source, model_source, exclude_var = NA){
-
+  function(data_source, model_source, exclude_var = NULL) {
     util_check_class("data_source", "data.frame")
 
-    util_check_class("model_source", "lm")
+    util_check_class("exclude_var", c("character", "NULL"))
 
-    util_check_class("exclude_var", c("character", "logical"))
-
-    crit <- qnorm((1 - 0.89) / 2, lower.tail = FALSE)
-
-    data_source %>%
-      dplyr::bind_cols(
-        predict(
-          model_source,
+    if (
+      "gam" %in% class(model_source)
+    ) {
+      raw_pred <-
+        marginaleffects::predictions(
+          model = model_source,
           newdata = data_source,
-          type = "response",
-          se.fit = TRUE,
-          exclude = exclude_var)) %>%
-      dplyr::mutate(
-        var = fit,
-        lwr = fit - (crit * se.fit),
-        upr = fit + (crit * se.fit)) %>%
+          exclude = exclude_var
+        )
+    } else {
+      raw_pred <-
+        marginaleffects::predictions(
+          model = model_source,
+          newdata = data_source
+        )
+    }
+
+    raw_pred %>%
+      dplyr::rename(
+        fit = predicted,
+        lwr = conf.low,
+        upr = conf.high,
+        sd_error = std.error
+      ) %>%
       dplyr::select(
-        !dplyr::any_of(
-          c("fit", "se.fit","df", "residual.scale")
-        ))  %>%
+        dplyr::any_of(
+          c(
+            names(data_source),
+            "fit",
+            "sd_error",
+            "lwr",
+            "upr"
+          )
+        )
+      ) %>%
+      tibble::as_tibble() %>%
       return()
   }
