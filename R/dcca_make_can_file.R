04cc75e31a9c245abdace455ca10d61c92add7e7
Ondrej Mottl <ondrej.mottl@gmail.com>
2022-09-23 16:28:13 +0200

    lint - mostly object names

--- R/dcca_make_can_file.R
+++ R/dcca_make_can_file.R
@@ -6,18 +6,18 @@
 #' @return
 #' FALSE on failure, TRUE otherwise
 #' @author Petr Smilauer
-dcca_export_can_file <-
-  function(path,
-           data) {
+dcca_make_can_file <-
+  function(sel_path,
+           sel_data) {
     n_rows <-
-      nrow(data)
+      nrow(sel_data)
 
     n_cols <-
-      ncol(data)
+      ncol(sel_data)
 
     # Try to create an empty file first
     if (
-      !file.create(path)
+      !file.create(sel_path)
     ) {
       return(FALSE)
     }
@@ -38,11 +38,11 @@ dcca_export_can_file <-
     id_x_line <- 4 # present line index ...
 
     for (i in 1:n_rows) {
-      a_line <- ""
+      sel_line <- ""
 
       for (j in 1:n_cols) {
-        a_line <-
-          paste(a_line, data[i, j])
+        sel_line <-
+          paste(sel_line, sel_data[i, j])
 
         if (
           (j %% 10) == 0
@@ -50,10 +50,10 @@ dcca_export_can_file <-
 
           # store current line state
           f_lines[id_x_line] <-
-            a_line
+            sel_line
 
           # reset to empty
-          a_line <- ""
+          sel_line <- ""
 
           # move to the next line index
           id_x_line <-
@@ -63,12 +63,12 @@ dcca_export_can_file <-
 
       # if we have something in the output buffer ...
       if (
-        nchar(a_line) > 0
+        nchar(sel_line) > 0
       ) {
 
         # store current line state
         f_lines[id_x_line] <-
-          a_line
+          sel_line
 
         # move to the next line index
         id_x_line <-
@@ -78,17 +78,17 @@ dcca_export_can_file <-
 
     # After data, store the names of variables
 
-    a_line <- ""
+    sel_line <- ""
 
     col_names <-
-      dimnames(data)[[2]]
+      dimnames(sel_data)[[2]]
 
     for (i in 1:n_cols) {
 
       # left-aligned, padded with space
-      a_line <-
+      sel_line <-
         paste( # no space among the labels
-          a_line,
+          sel_line,
           formatC(
             substring(col_names[i], 1, 8), # up to first 8 chars
             format = "s",
@@ -104,10 +104,10 @@ dcca_export_can_file <-
 
         # store current line
         f_lines[id_x_line] <-
-          a_line
+          sel_line
 
         # reset line to empty
-        a_line <- ""
+        sel_line <- ""
 
         # move to the next line index
         id_x_line <-
@@ -117,15 +117,15 @@ dcca_export_can_file <-
 
 
     if (
-      nchar(a_line) > 0 # something left in otput buffer
+      nchar(sel_line) > 0 # something left in otput buffer
     ) {
 
       # store current line
       f_lines[id_x_line] <-
-        a_line
+        sel_line
 
       # set buffer to empty
-      a_line <- ""
+      sel_line <- ""
 
       # progress to next line index
       id_x_line <-
@@ -133,14 +133,14 @@ dcca_export_can_file <-
     }
 
     row_names <-
-      dimnames(data)[[1]]
+      dimnames(sel_data)[[1]]
 
     for (i in 1:n_rows) {
 
       # see above for parameters
-      a_line <-
+      sel_line <-
         paste(
-          a_line,
+          sel_line,
           formatC(
             substring(row_names[i], 1, 8),
             width = 8,
@@ -154,9 +154,9 @@ dcca_export_can_file <-
         (i %% 10) == 0
       ) {
         f_lines[id_x_line] <-
-          a_line
+          sel_line
 
-        a_line <- ""
+        sel_line <- ""
 
         id_x_line <-
           id_x_line + 1
@@ -164,21 +164,21 @@ dcca_export_can_file <-
     }
 
     if (
-      nchar(a_line) > 0
+      nchar(sel_line) > 0
     ) {
       f_lines[id_x_line] <-
-        a_line
+        sel_line
     }
 
     # Open the file to be exported for writing
-    dta_1 <-
-      file(path, open = "wt")
+    temp_file <-
+      file(sel_path, open = "wt")
 
     # Output the lines
-    writeLines(f_lines, dta_1)
+    writeLines(f_lines, temp_file)
 
     # Close the file
-    close(dta_1)
+    close(temp_file)
 
     return(TRUE)
   }
