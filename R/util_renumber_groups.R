9d456112c61e5d6dd43d6c37b60497aeab65225d
Ondrej Mottl <ondrej.mottl@gmail.com>
2022-09-29 13:58:06 +0200

    rename and simplyfy

---
+++ R/util_renumber_groups.R
@@ -0,0 +1,11 @@
+#' @title  Re-number cluster groups
+#' @param clusgr Vector with integers
+#' @description Utility function to re-number vector of integers to simple
+#' sequence of numbers
+util_renumber_groups <-
+  function(clusgr) {
+    as.numeric(
+      as.factor(clusgr)
+    ) %>%
+      return()
+  }
