7d9ac5c2eb725af85b321e65b07e994f214e59c6
Ondrej Mottl <ondrej.mottl@gmail.com>
2023-04-10 21:07:51 +0200

    add_data_partition

--- R/add_data_partition.R
+++ R/add_data_partition.R
@@ -1,14 +1,20 @@
 #' @title Add partition of data
-#' @param data_source_var Data frame with the variables to partition
-#' @param data_source_levels Data frame with ages of levels
-#' @param age_var Name (in quotes) of variable which define the age of samples
-#' @description Add partition for each column for chronologically ordered data using regression trees
+#' @param data_source_var A dataframe with samples as rows and variables as
+#' columns. This dataframe must contain a "sample_id" column.
+#' @param data_source_levels A dataframe with one row per sample, where the
+#' sample_id column must match the data_source_var dataframe. This dataframe
+#' must contain an "age" column.
+#' @param age_var A character string indicating the name of the age column in
+#' the data_source_levels dataframe. Default value is "age".
+#' @description Add partition for each column for chronologically ordered
+#' data using regression trees.
+#' @return A dataframe with the same columns as data_source_var plus a column
+#' for each partition, named as variable name plus "_part".
 #' @export
 add_data_partition <-
   function(data_source_var,
            data_source_levels,
            age_var = "age") {
-
     RUtilpol::check_class("data_source_var", "data.frame")
 
     RUtilpol::check_class("data_source_levels", "data.frame")
@@ -19,38 +25,51 @@ add_data_partition <-
 
     RUtilpol::check_col_names("data_source_levels", c("sample_id", age_var))
 
+    # Join data_source_levels and data_source_var by the "sample_id" column
     data_work <-
       dplyr::inner_join(
         data_source_levels %>%
           dplyr::select(
+            # Select the sample_id and age columns
             dplyr::all_of(
               c("sample_id", eval(age_var))
-            )),
+            )
+          ),
         data_source_var %>%
+          # Select only the columns that are double
           dplyr::select(sample_id, where(is.double)),
-        by = "sample_id")
+        by = "sample_id"
+      )
 
+    # Pivot data to long format
     list_partitions <-
       data_work %>%
       tidyr::pivot_longer(
         cols = -c(sample_id, eval(age_var)),
         names_to = "type",
-        values_to = "estimate") %>%
+        values_to = "estimate"
+      ) %>%
+      # Split data by type column
       base::split(.$type) %>%
       purrr::map(
         .f = ~ regression_partition(
           data_source = .x,
           var = "estimate",
-          age_var = age_var))
+          age_var = age_var
+        )
+      )
 
+    # Bind the partitions to the original dataframe
     final_tibble <-
       dplyr::bind_cols(
         data_source_var,
+        # Select only the "rpart_groups" column from each partition
         list_partitions %>%
           purrr::map_dfc("rpart_groups") %>%
+          # Rename the columns to include the variable name and "_part"
           dplyr::rename_all(
-            .funs = ~ paste0(.x,"_part")))
-
+            .funs = ~ paste0(.x, "_part")
+          )
+      )
     return(final_tibble)
-
   }
